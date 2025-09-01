30b36b89c7ed221ad40ca1f3768d2b415ebe215f
microslash <elinalazarevamilana@gmail.com>
2025-09-01 21:40:53 +0900

    Create bot_demo.py

---
+++ bot_demo.py
@@ -0,0 +1,13 @@
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix='!')
+
+@bot.event
+async def on_ready():
+  print(f'Logged in as {bot_user}')
+
+@bot.command()
+  async def hello(ctx):
+    await ctx.send("Hello world")
+
